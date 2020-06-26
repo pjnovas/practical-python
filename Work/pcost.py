5079c4a4c11b59ccb0b7799a951f67e632222122
Pablo Novas <pjnovas@gmail.com>
2020-06-26 20:33:15 -0300

    1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,24 +2,29 @@
 #
 # Exercise 1.27
 
+import csv
+
 
 def portfolio_cost(filename):
     'Returns the total cost of the entire portfolio'
     total = 0
 
     f = open(filename, 'rt')
+    rows = csv.reader(f)
+    headers = next(rows)
 
-    next(f)  # read header
-
-    for line in f:
-        row = line.split(',')
+    for row in rows:
         try:
             total += int(row[1]) * float(row[2])
         except ValueError:
-            print(f'missing value on line {line}')
+            print(f'missing value on line {row}')
+
+    f.close()
 
     return total
 
 
 # cost = portfolio_cost('Data/portfolio.csv')
+# cost = portfolio_cost('Data/missing.csv')
+
 # print(f'Total cost {cost:0.2f}')
