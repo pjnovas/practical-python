ab158f1bccd9888e5ddcd0e861dd271f3a6998c6
Pablo Novas <pjnovas@gmail.com>
2020-06-26 20:28:20 -0300

    1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,16 +6,20 @@
 def portfolio_cost(filename):
     'Returns the total cost of the entire portfolio'
     total = 0
+
     f = open(filename, 'rt')
 
     next(f)  # read header
 
     for line in f:
         row = line.split(',')
-        total += int(row[1]) * float(row[2])
+        try:
+            total += int(row[1]) * float(row[2])
+        except ValueError:
+            print(f'missing value on line {line}')
 
     return total
 
 
-cost = portfolio_cost('Data/portfolio.csv')
-print(f'Total cost {cost:0.2f}')
+# cost = portfolio_cost('Data/portfolio.csv')
+# print(f'Total cost {cost:0.2f}')
