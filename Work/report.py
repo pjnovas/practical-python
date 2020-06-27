10b64b5cea60228c6ac3c413d6ec7358d8ce308c
Pablo Novas <pjnovas@gmail.com>
2020-06-27 18:05:11 -0300

    2.7

--- Work/report.py
+++ Work/report.py
@@ -34,3 +34,21 @@ def read_prices(filename):
                 prices[row[0]] = float(row[1])
 
     return prices
+
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+total_cost = 0.0
+current_value = 0.0
+
+for stock in portfolio:
+    total_cost += stock['share'] * stock['price']
+
+for stock in portfolio:
+    price = prices[stock['name']]
+    current_value += stock['share'] * price
+
+print('Total cost', total_cost)
+print('Current value', current_value)
+print(f'Gain/Loss {current_value - total_cost:0.2f}')
