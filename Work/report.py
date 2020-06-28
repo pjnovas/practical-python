94fb924145481a2721cb0e5b7d53ad706a43a622
Pablo Novas <pjnovas@gmail.com>
2020-06-28 12:55:00 -0300

    2.9

--- Work/report.py
+++ Work/report.py
@@ -15,7 +15,7 @@ def read_portfolio(filename):
         for row in rows:
             holding = {
                 'name': row[0],
-                'share': int(row[1]),
+                'shares': int(row[1]),
                 'price': float(row[2])
             }
             portfolio.append(holding)
@@ -36,19 +36,34 @@ def read_prices(filename):
     return prices
 
 
+def make_report(portfolio, prices):
+    report = []
+
+    for s in portfolio:
+        row = (s['name'], s['shares'],
+               prices[s['name']], prices[s['name']] - s['price'])
+        report.append(row)
+
+    return report
+
+
 portfolio = read_portfolio('Data/portfolio.csv')
 prices = read_prices('Data/prices.csv')
+report = make_report(portfolio, prices)
 
-total_cost = 0.0
-current_value = 0.0
+for r in report:
+    print(r)
 
-for stock in portfolio:
-    total_cost += stock['share'] * stock['price']
+# total_cost = 0.0
+# current_value = 0.0
 
-for stock in portfolio:
-    price = prices[stock['name']]
-    current_value += stock['share'] * price
+# for stock in portfolio:
+#     total_cost += stock['shares'] * stock['price']
 
-print('Total cost', total_cost)
-print('Current value', current_value)
-print(f'Gain/Loss {current_value - total_cost:0.2f}')
+# for stock in portfolio:
+#     price = prices[stock['name']]
+#     current_value += stock['shares'] * price
+
+# print('Total cost', total_cost)
+# print('Current value', current_value)
+# print(f'Gain/Loss {current_value - total_cost:0.2f}')
