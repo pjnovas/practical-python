c8ea1f5a8a024469f122a3804e2da0baf8adce1e
Pablo Novas <pjnovas@gmail.com>
2020-06-29 20:05:55 -0300

    3.2

--- Work/report.py
+++ Work/report.py
@@ -52,7 +52,11 @@ def print_report(report):
         print(f'{name:>10s} {shares:>10d} {p:>10s} {change:>10.2f}')
 
 
-portfolio = read_portfolio('Data/portfolio.csv')
-prices = read_prices('Data/prices.csv')
-report = make_report(portfolio, prices)
-print_report(report)
+def portfolio_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+    print_report(report)
+
+
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
