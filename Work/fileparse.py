28d2025a157e7ed47bce23ced1328fec2587124c
Pablo Novas <pjnovas@gmail.com>
2020-07-01 18:57:07 -0300

    3.5

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,35 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+
+def parse_csv(filename, select=None, types=None):
+    '''
+    Parse a CSV file into a list of records
+    '''
+
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        indices = [i for i, name in enumerate(headers)]
+
+        if select:
+            indices = [headers.index(name) for name in select]
+            headers = select
+
+        records = []
+        for row in rows:
+            if not row:    # Skip rows with no data
+                continue
+
+            row = [row[i] for i in indices]
+
+            if types:
+                row = [func(value) for func, value in zip(types, row)]
+
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
